c78bc03c4bc25137649825acc033d527d8d8163b
Ryan Davidson <52106678+rdsngit@users.noreply.github.com>
2025-11-04 17:18:12 +0000

    add #load_contacts_from_csv_file to ContactList

--- app/contact_list.rb
+++ app/contact_list.rb
@@ -10,4 +10,17 @@ class ContactList
   def initialize
     @contacts = []
   end
+
+  def load_contacts_from_csv_file(file_path)
+    CSV.foreach(file_path, headers: true) do |row|
+      contact = Contact.new(
+        name: row['name'],
+        email: row['email'],
+        phone: row['phone']
+      )
+      @contacts << contact
+    end
+
+    @contacts
+  end
 end
--- spec/contact_list_spec.rb
+++ spec/contact_list_spec.rb
@@ -13,4 +13,16 @@ RSpec.describe ContactList do
       expect(contact_list.contacts).to eq []
     end
   end
+
+  describe '#load_contacts_from_csv_file' do
+    it 'reads the input CSV file and for each row adds an instance of Contact with the name, email and phone data to the contacts array' do
+      contact_list.load_contacts_from_csv_file(input_csv_file_path)
+      expect(contact_list.contacts).to match_array([
+        have_attributes(class: Contact, name: 'John Doe', email: 'john@email.com', phone: '123-456-7890'),
+        have_attributes(class: Contact, name: 'Jane Smith', email: 'jane@email.com', phone: nil),
+        have_attributes(class: Contact, name: 'Bob Johnson', email: 'bob@invalid-email', phone: '555.123.4567'),
+        have_attributes(class: Contact, name: 'Mary Davis', email: 'mary@email.com', phone: '1234567890')
+      ])
+    end
+  end
 end
